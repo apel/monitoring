09a0b8cc5b9e2397ff02992877d2ca9f69e00468
Letizia <letizia.protopapa@stfc.ac.uk>
2023-01-06 11:39:55 +0000

    Ensure Python3 compatibility.

--- monitoring/publishing/views.py
+++ monitoring/publishing/views.py
@@ -21,14 +21,14 @@ class GridSiteViewSet(viewsets.ReadOnlyModelViewSet):
         last_fetched = GridSite.objects.aggregate(Max('fetched'))['fetched__max']
         # If there's no data then last_fetched is None.
         if last_fetched is not None:
-            print last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20)
+            print(last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20))
         if last_fetched is None or last_fetched.replace(tzinfo=None) < (datetime.today() - timedelta(hours=1, seconds=20)):
-            print 'Out of date'
+            print('Out of date')
             fetchset = VSuperSummaries.objects.using('grid').raw("SELECT Site, max(LatestEndTime) AS LatestPublish FROM VSuperSummaries WHERE Year=2019 GROUP BY 1;")
             for f in fetchset:
                 GridSite.objects.update_or_create(defaults={'updated': f.LatestPublish}, name=f.Site)
         else:
-            print 'No need to update'
+            print('No need to update')
 
         response = super(GridSiteViewSet, self).retrieve(request)
         date = response.data['updated'].replace(tzinfo=None)
@@ -59,14 +59,14 @@ class CloudSiteViewSet(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request):
         last_fetched = CloudSite.objects.aggregate(Max('fetched'))['fetched__max']
-        print last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20)
+        print(last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20))
         if last_fetched.replace(tzinfo=None) < (datetime.today() - timedelta(hours=1, seconds=20)):
-            print 'Out of date'
+            print('Out of date')
             fetchset =  VAnonCloudRecord.objects.using('cloud').raw("SELECT b.SiteName, COUNT(DISTINCT VMUUID) as VMs, CloudType, b.UpdateTime FROM (SELECT SiteName, MAX(UpdateTime) AS latest FROM VAnonCloudRecords WHERE UpdateTime>'2018-07-25' GROUP BY SiteName) AS a INNER JOIN VAnonCloudRecords AS b ON b.SiteName = a.SiteName AND b.UpdateTime = a.latest GROUP BY SiteName")
             for f in fetchset:
                 CloudSite.objects.update_or_create(defaults={'vms': f.VMs, 'script': f.CloudType, 'updated': f.UpdateTime}, name=f.SiteName)
         else:
-            print 'No need to update'
+            print('No need to update')
 
         response = super(CloudSiteViewSet, self).list(request)
         # Wrap data in a dict so that it can display in template.
@@ -76,14 +76,14 @@ class CloudSiteViewSet(viewsets.ReadOnlyModelViewSet):
 
     def retrieve(self, request, pk=None):
         last_fetched = CloudSite.objects.aggregate(Max('fetched'))['fetched__max']
-        print last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20)
+        print(last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20))
         if last_fetched.replace(tzinfo=None) < (datetime.today() - timedelta(hours=1, seconds=20)):
-            print 'Out of date'
+            print('Out of date')
             fetchset =  VAnonCloudRecord.objects.using('cloud').raw("SELECT b.SiteName, COUNT(DISTINCT VMUUID) as VMs, CloudType, b.UpdateTime FROM (SELECT SiteName, MAX(UpdateTime) AS latest FROM VAnonCloudRecords WHERE UpdateTime>'2018-07-25' GROUP BY SiteName) AS a INNER JOIN VAnonCloudRecords AS b ON b.SiteName = a.SiteName AND b.UpdateTime = a.latest GROUP BY SiteName")
             for f in fetchset:
                 CloudSite.objects.update_or_create(defaults={'vms': f.VMs, 'script': f.CloudType, 'updated': f.UpdateTime}, name=f.SiteName)
         else:
-            print 'No need to update'
+            print('No need to update')
 
         response = super(CloudSiteViewSet, self).retrieve(request)
         # Wrap data in a dict so that it can display in template.
