2a2caea72442b0903ff645e132f0a62f88afc312
Letizia <letizia.protopapa@stfc.ac.uk>
2023-01-25 15:52:27 +0000

    Add serialiser for the table with submithosts.

--- monitoring/publishing/serializers.py
+++ monitoring/publishing/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from monitoring.publishing.models import CloudSite, GridSite, GridSiteSync
+from monitoring.publishing.models import CloudSite, GridSite, GridSiteSync, GridSiteSyncSubmitH
 
 
 class GridSiteSerializer(serializers.HyperlinkedModelSerializer):
@@ -39,3 +39,15 @@ class CloudSiteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CloudSite
         fields = ('url', 'SiteName', 'Vms', 'Script', 'updated')
+
+
+class GridSiteSyncSubmitHSerializer(serializers.HyperlinkedModelSerializer):
+    # Override default format with None so that Python datetime is used as
+    # ouput format. Encoding will be determined by the renderer and can be
+    # formatted by a template filter.
+
+    class Meta:
+        model = GridSiteSyncSubmitH
+        fields = ('url', 'SiteName', 'YearMonth', 'RecordStart', 'RecordEnd', 'RecordCountPublished', 'RecordCountInDb', 'SubmitHost')
+        
+        lookup_fields = ('SiteName', 'YearMonth')
