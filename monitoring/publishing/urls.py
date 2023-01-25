b37dde27df3386f3d455234a701c222453156a15
Letizia <letizia.protopapa@stfc.ac.uk>
2023-01-25 16:04:30 +0000

    Add url to the page showing submithosts.

--- monitoring/publishing/urls.py
+++ monitoring/publishing/urls.py
@@ -9,10 +9,11 @@ router = routers.SimpleRouter()
 router.register(r'cloud', views.CloudSiteViewSet)
 router.register(r'grid', views.GridSiteViewSet)
 router.register(r'gridsync', views.GridSiteSyncViewSet)
+router.register(r'gridsync', views.GridSiteSyncSubmitHViewSet)
 
-# Necessary to retrieve one site only
 urlpatterns = [
     re_path(r'^gridsync/(?P<SiteName>[a-zA-Z0-9-]+)/$', views.GridSiteSyncViewSet.as_view({'get': 'retrieve'}), name='gridsync_singlesite'),
+    re_path(r'^gridsync/(?P<SiteName>[a-zA-Z0-9-]+)/(?P<YearMonth>[0-9-]+)/$', views.GridSiteSyncSubmitHViewSet.as_view({'get': 'retrieve'}), name='gridsync_submithost'),
 ]
 
 urlpatterns += router.urls
