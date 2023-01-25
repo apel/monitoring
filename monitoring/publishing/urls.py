acecbd3d612c44b3f0d7cab7cf7fa56da8c2e9d0
Letizia <letizia.protopapa@stfc.ac.uk>
2023-01-25 11:11:28 +0000

    Add urls for gridsync pages.

--- monitoring/publishing/urls.py
+++ monitoring/publishing/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from monitoring.publishing import views
+from django.urls import re_path
 
 router = routers.SimpleRouter()
 router.register(r'cloud', views.CloudSiteViewSet)
 router.register(r'grid', views.GridSiteViewSet)
+router.register(r'gridsync', views.GridSiteSyncViewSet)
 
-
+# Necessary to retrieve one site only
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    re_path(r'^gridsync/(?P<SiteName>[a-zA-Z0-9-]+)/$', views.GridSiteSyncViewSet.as_view({'get': 'retrieve'}), name='gridsync_singlesite'),
 ]
+
+urlpatterns += router.urls
