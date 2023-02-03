2d3f9610d9522b0a12ac0acc3a7305614ae08927
Letizia <letizia.protopapa@stfc.ac.uk>
2023-02-03 13:05:43 +0000

    Move import to the top of file.

--- monitoring/availability/urls.py
+++ monitoring/availability/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
 import sys
-sys.path.append('/usr/share/DJANGO_MONITORING_APP')
 from monitoring.availability import views
+
+sys.path.append('/usr/share/DJANGO_MONITORING_APP')
 
 urlpatterns = [
     url(r'^$', views.status),
