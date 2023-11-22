3b3453db70d6859f03cdccc07be8878b0f1aac1f
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2023-11-22 11:56:47 +0000

    Automate venv activation
    Putting venv activation in the wsgi file means that Apache can invoke
    the venv and it doesn't need to be done manually.

--- monitoring/wsgi.py
+++ monitoring/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+# Activate virtualenv
+activate_path = os.path.expanduser("/usr/share/DJANGO_MONITORING_APP/venv/bin/activate_this.py")
+with open(activate_path) as act:
+    exec(act.read(), dict(__file__=activate_path))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monitoring.settings")
 
 application = get_wsgi_application()
